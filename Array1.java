19b527641660a827276b71e3e6da774955493123
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-06-27 21:52:15 +0530

    basic Array Learning with examples

---
+++ Array1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] arg) {
+
+        int [] age = {1,5,8,45,89};
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+        System.out.println(age[3]);
+        System.out.println(age[4]);
+
+    }
+}
---
+++ Array2ByForLoop.java
@@ -0,0 +1,9 @@
+public class Array2ByForLoop {
+    public static void main(String[]arg){
+        int [] Roll = {1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<Roll.length;i++)
+        {
+            System.out.println(Roll[i]);
+        }
+    }
+}
