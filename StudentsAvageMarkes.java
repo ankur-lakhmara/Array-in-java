666bf6a600812b06f3222a9d558f905354e7ada4
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-06-28 11:10:17 +0530

    Update StudentsAvageMarkes.java

--- StudentsAvageMarkes.java
+++ StudentsAvageMarkes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StudentsAvageMarkes {
+public class StudentsAvageMarks {
     public static void main(String[]arg){
         System.out.println("Enter Total number of students");
         Scanner sc = new Scanner(System.in);
